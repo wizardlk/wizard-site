a864d51c6fe01065529994442f8ffdb7fd45d4d2
Gayan <kanushkanet@gmail.com>
2019-03-31 10:13:34 +0530

    customize admin view

--- posts/admin.py
+++ posts/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 
 from .models import Video
 
-admin.site.register(Video)
+class VideoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'published_at', 'was_published_recently')
+    list_filter = ['published_at']
+
+admin.site.register(Video, VideoAdmin)
