06352299169e2bcfe0818e3349e5255a3acc628b
Gayan <kanushkanet@gmail.com>
2019-03-31 10:13:11 +0530

    update Video model with new urls

--- posts/models.py
+++ posts/models.py
@@ -5,15 +5,18 @@ from django.utils import timezone
 
 class Video(models.Model):
     title = models.CharField(max_length=100)
-    short_description = models.CharField(max_length=250, blank=True)
-    full_description = models.TextField(blank=True)
-    link = models.CharField(max_length=250)
-    published_at = models.DateTimeField('date published')
-    created_at = models.DateTimeField('date created')
-    updated_at = models.DateTimeField('date updated')
+    short_description = models.CharField(max_length=250, blank=True, default='')
+    full_description = models.TextField(blank=True, default='')
+    watch_link = models.CharField('video watch url', max_length=250, default='')
+    embed_link = models.CharField('video embed url', max_length=250, default='')
+    published_at = models.DateTimeField('date published', default=timezone.now)
+    created_at = models.DateTimeField('date created', auto_now_add=True)
+    updated_at = models.DateTimeField('date updated', auto_now=True)
 
     def __str__(self):
         return self.title
     
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=5)
+        return self.published_at >= timezone.now() - datetime.timedelta(days=5)
+    
+    was_published_recently.boolean = True
