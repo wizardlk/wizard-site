549b71b531643b06ca68dad8a2a9ff652d61394e
Gayan <kanushkanet@gmail.com>
2019-05-18 21:56:24 +0530

    update social links

--- posts/static/posts/js/posts/scripts.js
+++ posts/static/posts/js/posts/scripts.js
@@ -13,17 +13,7 @@
 		}
 		hidePreloader();
 	});
-
 	
-	/* Navbar Scripts */
-	// jQuery to collapse the navbar on scroll
-	// $(window).scroll(function() {
-	// 	if ($(".navbar").offset().top > 60) {
-	// 		$(".fixed-top").addClass("top-nav-collapse");
-	// 	} else {
-	// 		$(".fixed-top").removeClass("top-nav-collapse");
-	// 	}
-	// });
 
 	// jQuery for page scrolling feature - requires jQuery Easing plugin
 	$(function() {
